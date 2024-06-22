13626a4dbeac550b6eba558d3a6f9cab1be4614c
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-06-23 01:58:52 +0530

    Prisma

---
+++ packages/db/prisma/migrations/20240622195245_/migration.sql
@@ -0,0 +1,24 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - Added the required column `hashedPassword` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `userName` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "hashedPassword" TEXT NOT NULL,
+ADD COLUMN     "userName" TEXT NOT NULL;
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Session" ADD CONSTRAINT "Session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
