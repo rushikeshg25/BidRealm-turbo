2ca6c0b91fa47c53d896372f1bda4a9bf4b91dd2
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-06-23 12:45:58 +0530

    feat: Ws server config

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -10,11 +10,7 @@ wss.on("connection", function connection(ws) {
   ws.on("error", console.error);
 
   ws.on("message", function message(data, isBinary) {
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data, { binary: isBinary });
-      }
-    });
+    wss.clients.forEach(function each(client) {});
   });
 
   ws.send("Hello! Message From Server!!");
