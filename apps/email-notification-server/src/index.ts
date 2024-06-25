7332b36140830a5ab8a9c03812a8160f8f99e60a
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-06-25 18:07:04 +0530

    feat: Email Service Boilerplate

--- apps/email-notification-server/src/index.ts
+++ apps/email-notification-server/src/index.ts
@@ -1,14 +1,33 @@
-import redis from "redis";
-import nodemailer from "nodemailer";
+import { createClient } from "redis";
 
-const client = redis.createClient();
-
-const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
-  auth: {
-    user: "email@gmail.com",
-    pass: "password",
+const client = createClient({
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: 17801,
   },
 });
+client.on("error", (err) => console.log("Redis Client Error", err));
+
+async function startWorker() {
+  try {
+    await client.connect();
+    console.log("Worker connected to Redis.");
+
+    // Main loop
+    while (true) {
+      try {
+        const EmailData = await client.brPop("problems", 0);
+        //Logic to send email
+      } catch (error) {
+        console.error("Error processing submission:", error);
+        // Implement your error handling logic here. For example, you might want to push
+        // the submission back onto the queue or log the error to a file.
+      }
+    }
+  } catch (error) {
+    console.error("Failed to connect to Redis", error);
+  }
+}
+
+startWorker();
