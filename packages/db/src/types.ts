378938764087e212276a4f12f34f192bd5bcaf3c
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-06-28 01:33:26 +0530

    feat: exported types generated by prisma client in DB package

---
+++ packages/db/src/types.ts
@@ -0,0 +1,6 @@
+import { Bid, Auction, AuctionStatus, User } from "@prisma/client";
+
+export type BidT = Bid;
+export type AuctionT = Auction;
+export type AuctionStatusT = AuctionStatus;
+export type UserT = User;
