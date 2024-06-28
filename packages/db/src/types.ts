95bb85d3ecf910f94d1d740f1a09d4a178450538
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-06-28 14:31:08 +0530

    feat: complex queries types exported

--- packages/db/src/types.ts
+++ packages/db/src/types.ts
@@ -1,6 +1,39 @@
-import { Bid, Auction, AuctionStatus, User } from "@prisma/client";
+import { Bid, Auction, AuctionStatus, User, Prisma } from "@prisma/client";
+
+import prisma from "./index";
 
 export type BidT = Bid;
 export type AuctionT = Auction;
 export type AuctionStatusT = AuctionStatus;
 export type UserT = User;
+
+export type AuctionWithBidsandUserT = Prisma.AuctionGetPayload<{
+  include: {
+    bids: true;
+    user: true;
+  };
+}>;
+
+export type AuctionWithBidsT = Prisma.AuctionGetPayload<{
+  include: {
+    bids: true;
+  };
+}>;
+
+export type AuctionWithUserT = Prisma.AuctionGetPayload<{
+  include: {
+    user: true;
+  };
+}>;
+
+export type UserwithAuctionT = Prisma.UserGetPayload<{
+  include: {
+    auctions: true;
+  };
+}>;
+
+export type UserwithBidsT = Prisma.UserGetPayload<{
+  include: {
+    bids: true;
+  };
+}>;
