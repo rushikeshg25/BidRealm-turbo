ad45de496e27ee87c23e5cd43e42d9cb0ab7c236
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-07-01 14:17:49 +0530

    feat: Menu on Mobile

--- apps/client/components/Navbar.tsx
+++ apps/client/components/Navbar.tsx
@@ -1,12 +1,15 @@
 "use client";
 import React from "react";
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 import { Input } from "@/components/ui/input";
 import GavelIcon from "./icons/GavelIcon";
 import SearchIcon from "./icons/SearchIcon";
 import UserIcon from "./icons/UserIcon";
 import { ModeToggle } from "./ThemeToggle";
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -20,6 +23,14 @@ import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 import { useMutation } from "@tanstack/react-query";
 import Signout from "@/actions/auth/Signout";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 const Navbar = ({
   className,
@@ -28,6 +39,8 @@ const Navbar = ({
   className?: string;
   session: Session | null;
 }) => {
+  const pathname = usePathname();
+
   const router = useRouter();
   const { mutate: server_Signout } = useMutation({
     mutationFn: Signout,
@@ -35,6 +48,9 @@ const Navbar = ({
       router.push("/");
     },
   });
+  const LoginHandler = () => {
+    router.push("/sign-up");
+  };
   const LogoutHandler = () => {
     server_Signout();
   };
@@ -56,30 +72,32 @@ const Navbar = ({
           />
         </div>
       </div>
-      <nav className='items-center hidden gap-4 md:flex'>
-        <Link
-          href='http://localhost:3000/live-auctions?status=active'
-          className='font-medium hover:text-primary-foreground/80 dark:hover:text-primary-foreground'
-          prefetch={false}
-        >
-          Live Auctions
-        </Link>
-        <Link
-          href='http://localhost:3000/live-auctions?status=active'
-          className='font-medium hover:text-primary-foreground/80 dark:hover:text-primary-foreground'
-          prefetch={false}
-        >
-          Upcoming
-        </Link>
-        <Link
-          href='#'
-          className='font-medium hover:text-primary-foreground/80 dark:hover:text-primary-foreground'
-          prefetch={false}
-        >
-          Closed
-        </Link>
-      </nav>
-      <div className='flex items-center gap-4'>
+      {pathname === "/" && (
+        <nav className='items-center hidden gap-4 md:flex'>
+          <Link
+            href='http://localhost:3000/live-auctions?status=active'
+            className='font-medium hover:text-primary-foreground/80 dark:hover:text-primary-foreground'
+            prefetch={false}
+          >
+            Live Auctions
+          </Link>
+          <Link
+            href='http://localhost:3000/live-auctions?status=active'
+            className='font-medium hover:text-primary-foreground/80 dark:hover:text-primary-foreground'
+            prefetch={false}
+          >
+            Upcoming
+          </Link>
+          <Link
+            href='#'
+            className='font-medium hover:text-primary-foreground/80 dark:hover:text-primary-foreground'
+            prefetch={false}
+          >
+            Closed
+          </Link>
+        </nav>
+      )}
+      <div className='hidden md:flex items-center gap-4'>
         <ModeToggle />
         {!session ? (
           <Button onClick={() => router.push("/sign-up")}>Sign Up</Button>
@@ -108,6 +126,68 @@ const Navbar = ({
             </DropdownMenu>
           </>
         )}
+      </div>
+      {/*Mobile Menu*/}
+      <div className='md:hidden'>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant='outline' className='w-full h-full '>
+              <Menu />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>BidRealm</SheetTitle>
+            </SheetHeader>
+
+            <SheetClose asChild className='h-full pt-10'>
+              <div className='flex flex-col items-center justify-center gap-y-2'>
+                {session !== null && (
+                  <Button variant={"ghost"} onClick={() => router.push("/new")}>
+                    New Auction
+                  </Button>
+                )}
+                {session !== null && (
+                  <Button
+                    variant={"ghost"}
+                    onClick={() => router.push("/my-auctions")}
+                  >
+                    My Auctions
+                  </Button>
+                )}
+
+                {session !== null && (
+                  <Button
+                    variant={"ghost"}
+                    onClick={() => router.push("/my-bids")}
+                  >
+                    Bids
+                  </Button>
+                )}
+
+                {session !== null ? (
+                  <Button variant={"ghost"} onClick={LogoutHandler}>
+                    Logout
+                  </Button>
+                ) : (
+                  <Button variant={"ghost"} onClick={LoginHandler}>
+                    SignUp
+                  </Button>
+                )}
+                <div className='flex-grow'></div>
+                <div className='flex items-center gap-2 mb-9'>
+                  <Link href={"https://github.com/rushikeshg25/bid-turbo"}>
+                    <Button variant='outline'>
+                      <GitHubLogoIcon className='w-5 h-5 mr-2' />
+                      Github
+                    </Button>
+                  </Link>
+                  <ModeToggle />
+                </div>
+              </div>
+            </SheetClose>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
