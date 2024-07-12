8b819162c474857e9f83c2547bf64f9af7e71b20
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-07-13 02:21:12 +0530

    fix:auctions page style

--- apps/client/components/pages/Auctions.tsx
+++ apps/client/components/pages/Auctions.tsx
@@ -21,7 +21,7 @@ type AuctionT = {
 
 const Auctions = ({ auctions }: { auctions: AuctionT[] }) => {
   return (
-    <section className="grid grid-cols-1 gap-4 p-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:p-6">
+    <section className='grid grid-cols-1 gap-4 p-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 '>
       {auctions.map((auction) => (
         <AuctionCardComponent key={auction.id} auction={auction} />
       ))}
